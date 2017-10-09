b5c9a552c8a5bc8beb77bef8b46bdd6269310dea
Gabriel Kramer-Garcia <glk2110@columbia.edu>
2017-10-08 22:53:46 -0400

    fe

--- searcher.py
+++ searcher.py
@@ -24,29 +24,27 @@ def printFeedback(query, precision, relevants):
 		return 0
 	return 1
 
-def getNewQuery(oldQuery, yWords, nWords):
+def getNewQuery(oldQuery, allWords):
 	allWords = []
 	realWords = []
 	stopWords = ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"]
-	for word in yWords:
-		allWords.append(word)
-	for word2 in nWords:
-		allWords.append(word2)
-	for string1 in allWords:
-		exclude = set(string.punctuation)
-		temp = ''.join(ch for ch in string1 if ch not in exclude)
-		tokens = nltk.word_tokenize(temp)
-		for word3 in tokens:
-			word3 = word3.lower()
-			if word3 not in stopWords:
-				if word3 != "":
-					realWords.append(word3)
+	for string2 in allWords:
+		realWords1 = []
+		for string1 in string2:
+			exclude = set(string.punctuation)
+			temp = ''.join(ch for ch in string1 if ch not in exclude)
+			tokens = nltk.word_tokenize(temp)
+			for word3 in tokens:
+				word3 = word3.lower()
+				if word3 not in stopWords:
+					if word3 != "":
+						realWords1.append(word3)
+		realWords.append(realWords1)
 	wordList = []
 	for i in realWords:
-		if i not in wordList:
-			wordList.append(i)
-		else:
-			print(i)
+		for j in i:
+			if j not in wordList:
+				wordList.append(i)
 	print(realWords)
 	print(wordList)
 	return oldQuery
@@ -67,10 +65,12 @@ def makeQuery(apiKey, engineID, precision, search):
 	print("Precision 	= " + str(precision))
 	print("Google Search Results:")
 	print("======================")
+	global relevants
 	relevants = []
 	relevantWords = []
 	badWords = []
 	for i in range(10):
+		temp = []
 		print("Result " + str(i + 1) + "\n")
 		solution = str(res[u'items'][i][u'link'])
 		title =  str(res[u'items'][i][u'title'])
@@ -81,20 +81,13 @@ def makeQuery(apiKey, engineID, precision, search):
 		print("\nIs this Relevant (Y/N)?")
 		good = raw_input()
 		relevants.append(good)
-		if good:
-			if good[0] == "y" or good[0] == "Y":
-				relevantWords.append(title)
-				relevantWords.append(summary.encode('ascii','ignore'))
-			else:
-				badWords.append(title)
-				badWords.append(summary.encode('ascii','ignore'))
-		else:
-			badWords.append(title)
-			badWords.append(summary)
+		temp.append(title)
+		temp.append(summary.encode('ascii','ignore'))
+		relevantWords.append(temp)
 	printedFeedback = printFeedback(search, float(precision), relevants)
 	if printedFeedback == 0:
 		print("Still below the desired precision of " + str(float(precision)) + "\nExpanding Query\n")
-		newSearch = getNewQuery(search, relevantWords, badWords)
+		newSearch = getNewQuery(search, relevantWords)
 		makeQuery(apiKey, engineID, precision, newSearch)
 	elif printedFeedback == 1:
 		print("Desired precision reached, done")
